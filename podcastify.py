d1dda8bd6dabd0c40f68b23b1c4db87ef494eb38
Tim Godfrey <tim@wholebaked.com.au>
2012-10-23 13:03:48 +1100

    Added some instructionals as to what podcastify actually does

--- podcastify.py
+++ podcastify.py
@@ -6,10 +6,14 @@ from actionify import OffworldShowAction
 
 if __name__ == '__main__':
     PROJECT_PATH = os.path.dirname(os.path.realpath(__file__))
+    # Step 1: Make soup from feed
     soupified = SoupifyFeed('http://www.offworldrecordings.com/feed/')
+    # Step 2: Filter feed if necessary
     rss_filter = RssSoupFilter(
         soupified.get_soup(), title_all_keywords=['offworld', 'show'])
     rss_filter.filter()
+    # Step 3: Perform an action on the feed. In this case, we're scraping the
+    # blog page for the offworld show download and tracklist
     action = OffworldShowAction(
         items=rss_filter.get_items(), save_path=PROJECT_PATH)
     action.run()
